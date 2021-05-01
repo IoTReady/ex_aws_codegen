e06271311d5134fccc84e6ad08eefbae8b66d6ea
Tej Pochiraju <tej@iotready.co>
2021-05-01 13:27:47 +0530

    Working jinja based code

--- generate.py
+++ generate.py
@@ -3,13 +3,10 @@ import requests
 import json
 import os
 import re
+import subprocess
+from jinja2 import Template
 
 default_template = 'template.ex'
-
-all_types_strings = {}
-all_types = {}
-
-built_in_types = ['binary', 'boolean', 'float', 'integer', 'map', 'string', 'number', 'port', 'error_message']
 
 @click.group()
 def cli():
@@ -23,16 +20,20 @@ def cli():
 def parse(template, modulename, url):
     """Download JSON from URL and parse."""
     output_file = get_snake_case(modulename) + '.ex'
-    with open(template, 'r') as readfile:
-        doc = readfile.read()
     response = requests.get(url).json()
-    doc = doc.replace('$modulename', modulename)
-    doc = insert_version(doc, version = response['version'])
-    doc = insert_metadata(doc, response['metadata'])
-    doc = insert_types(doc, response['shapes'])
-    doc = insert_functions(doc, response['operations'], response['shapes'])
-    with open(output_file, 'w') as writefile:
-        writefile.write(doc)
+    context = {}
+    context['modulename'] = modulename
+    context['version'] = response['version']
+    context['metadata'] = response['metadata']
+    context['servicename'] = get_snake_case(response['metadata']['endpointPrefix'])
+    context['types'] = parse_shapes(response['shapes'])
+    context['functions'] = parse_operations(response['operations'], context['types'])
+    with open(template, 'r') as f:
+        jinja_template = Template(f.read())
+        out = jinja_template.render(context)
+        with open(output_file, 'w') as wf:
+            wf.write(out)
+    subprocess.run(["mix", "format", output_file])
 
 def get_snake_case(str):
     return re.sub('([a-z]+)([A-Z])', r'\1_\2', str).replace('.', '_').lower()
@@ -60,104 +61,76 @@ def get_http_method(http_method):
     }
     return methods_mapping[http_method]
 
-def insert_version(doc, version):
-    return doc.replace('$version', version)
-
-def insert_metadata(doc, metadata):
-    metadata_string = ''
-    for key, value in metadata.items():
-        metadata_string += '#\t{}: {}\n\t'.format(key, value)
-    doc = doc.replace('$metadata', metadata_string)
-    service_name = get_snake_case(metadata['endpointPrefix'])
-    return doc.replace('$service', ':{}'.format(service_name))
-
-def insert_types(doc, shapes, only_required = True):
-    type_template = """\n\t@type $type_name :: $type_value\n"""
-    all_types_string = ''
+def parse_shapes(shapes, only_required = False):
+    all_types = {}
+    built_in_types = ['binary', 'boolean', 'float', 'integer', 'map', 'string', 'number', 'port']
     complex_types = ['list', 'structure']
     for k,v in shapes.items():
         type_name = get_snake_case(k)
         if type_name in built_in_types or type_name in all_types:
             # Do not redefine elixir's built-in types or previously defined types
             continue
-        type_string = type_template.replace('$type_name', type_name)
         aws_type = v['type']
         if aws_type not in complex_types:
-            # basic types
-            elixir_type = get_elixir_type(aws_type)
-            type_string = type_string.replace('$type_value', elixir_type)
-            all_types[type_name] = elixir_type
+            # basic shapes
+            all_types[type_name] = {'shape': 'basic', 'def': get_elixir_type(aws_type)}
         elif aws_type == 'list':
             element_type = v['member']['shape']
-            elixir_type = get_elixir_type(element_type)
-            type_string = type_string.replace('$type_value', '[{}]'.format(elixir_type))
-            all_types[type_name] = elixir_type
+            all_types[type_name] = {'shape': 'list', 'def': get_elixir_type(element_type)}
         elif aws_type == 'structure':
-            all_types[type_name] = {}
-            if only_required:
-                if v.get('required'):
-                    struct = '%{'
-                    for key in v['required']:
-                        value = get_snake_case(v['members'][key]['shape'])
-                        struct += key + ': ' + value + ', '
-                        all_types[type_name][key] = value
-                    struct = struct[:-2] + '}'
-                else:
-                    struct = '%{}'
-            else:
-                if len(v['members']):
-                    struct = '%{'
-                    for key,b in v['members'].items():
-                        value = get_snake_case(b['shape'])
-                        struct += key + ': ' + value + ', '
-                    struct = struct[:-2] + '}'
-                else:
-                    struct = '%{}'
-            type_string = type_string.replace('$type_value', struct)
-            all_types_strings[type_name] = struct
+            all_types[type_name] = {'shape': 'structure', 'def': {}}
+            keys = []
+            if only_required and v.get('required'):
+                keys = v['required']
+            elif len(v['members']):
+                keys = list(v['members'].keys())
+            for key in keys:
+                try:
+                    value = get_snake_case(v['members'][key]['shape'])
+                    all_types[type_name]['def'][key] = value
+                except:
+                    print("keys", keys)
+                    print(k,v, key)
         else:
             print(k,v)
             continue
-        all_types_string += type_string
-    return doc.replace('$types', all_types_string)
+        
+    return all_types
 
-def get_param_name(input_type, param_id):
+def get_param_name(all_types, input_type, param_id):
     try:
-        return all_types[input_type][param_id]
+        return all_types[input_type]['def'][param_id]
     except:
-        print(input_type, param_id, all_types[input_type])
-        return 'certificate_id'
+        # Edge case for IoT service
+        # param is named 'caCertificateId' but the definition uses 'certificate_id'
+        # print(input_type, param_id, all_types[input_type])
+        if param_id == 'caCertificateId':
+            return 'certificate_id'
+        else:
+            print(input_type, param_id, all_types[input_type])
+            return ''
 
-def insert_functions(doc, operations, shapes):
-    function_template = """\n\t@spec $func_name($input_type) :: $output_type\n\tdef $func_name($param_list = params) do\n\t%{@query | http_method: $http_method, path: "$uri", params: params} |> execute()\n\tend\n"""
-    all_functions = ''
+def parse_operations(operations, all_types):
+    all_functions = {}
     for op in operations.values():
         func_name = get_snake_case(op['name'])
-        func_string = function_template.replace('$func_name', func_name)
-        output_type = op.get('output')
-        if output_type:
-            output_type = get_snake_case(output_type['shape'])
-            func_string = func_string.replace('$output_type', output_type)
-        else:
-            func_string = func_string.replace('$output_type', 'nil')
         input_type = get_snake_case(op['input']['shape'])
-        func_string = func_string.replace('$input_type', input_type)
-        func_string = func_string.replace('$param_list', all_types_strings[input_type])
-        http_method = op['http']['method']
-        func_string = func_string.replace('$http_method', get_http_method(http_method))
-        uri = op['http']['requestUri']
+        output_type = (op.get('output') and get_snake_case(op['output']['shape'])) or "nil"
         # not sure why some uris have a + in the param list (for multiple?)
-        uri = uri.replace('+', '')
-        uri = re.split("[{}]", uri)
-        uri = [part for part in uri if part]
-        uri = ["#{"+get_param_name(input_type, part)+"}" if not part.startswith('/') else part for part in uri]
+        # This trips up the Elixir compiler - convert to list?
+        uri = op['http']['requestUri'].replace('+', '')
+        # clean out empty strings and isolate params
+        uri = [part for part in re.split("[{}]", uri) if part]
+        uri = ["#{"+get_param_name(all_types, input_type, param_id)+"}" if not param_id.startswith('/') else param_id for param_id in uri]
         uri = ''.join(uri)
-        func_string = func_string.replace('$uri', uri)
-        all_functions += func_string
-
-    return doc.replace('$functions', all_functions)
-
+        all_functions[func_name] = {
+            'input_type': input_type,
+            'output_type': output_type,
+            'params': all_types[input_type]['def'],
+            'http_method': get_http_method(op['http']['method']),
+            'uri': uri,
+        }
+    return all_functions
 
 if __name__ == '__main__':
     cli()
-    print(all_types)
--- template.ex
+++ template.ex
@@ -1,19 +1,31 @@
-defmodule $modulename do
+defmodule {{modulename}} do
 
-  # version: $version
-  $metadata
+  # version: {{version}}
+  {% for key,value in metadata.items() %}
+  # {{key}}: {{value}}
+  {%- endfor %}
 
-  @service $service
+  @service :{{servicename}}
+
+  {% for key,value in types.items() %}
+  {% if value.shape == 'basic' %}
+  @type {{key}} :: {{value.def}}
+  {% elif value.shape == 'list' %}
+  @type {{key}} :: [{{value.def}}]
+  {% elif value.shape == 'structure' %}
+  @type {{key}} :: %{{value.def | tojson | replace('"', '') }}
+  {% endif %}
+  {% endfor %}
+
 
   @query %ExAws.Operation.RestQuery {service: @service}
 
-  $types
-
-  $functions
-
-  defp execute(query) do
-    {:ok, %{status_code: 200, body: body}} = query |> ExAws.request(service_override: :"execute-api")
-    body |> Jason.decode!()
+  {% for key, value in functions.items() %}
+  @spec {{key}}({{value.input_type}}) :: {{value.output_type}}
+  def {{key}}(%{{value.params | tojson | replace('"', '') }} = params) do
+    %{@query | http_method: {{value.http_method}}, path: "{{value.uri}}", params: params}
   end
+  {% endfor %}
+
 
 end
